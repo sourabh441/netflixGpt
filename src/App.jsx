389e05423098b6cce007448a00693f8e96c5c619
sourabh <sahilsahita441@gmail.com>
2025-06-27 04:45:49 +0530

    Sign IN and Sign Up form and Some header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+
+import Body from './components/Body'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-     
-      <h1 className='text-bold text-red-500 underline'>Vite + React</h1>
-      
-    </>
+   <Body/>
   )
 }
 
